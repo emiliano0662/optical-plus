7e561cc68d10e711b9d740f92b1f7582f124a3bd
Emiliano Carreño <emiliano0662@gmail.com>
2021-11-24 20:36:30 -0500

    avances

--- src/app.js
+++ src/app.js
@@ -12,30 +12,8 @@ var wow = new WOW({
     scrollContainer: null
 });
 
-$('header').load("header.html");
-$('footer').load("footer.html");
-
-/*Script para el slider banner*/
-if ($.contains(document.body, document.getElementById('slider-home-banner'))) {
-
-    var mySwiperOurBeliefs = new Swiper('#slider-home-banner', {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 0,
-        autoplay: {
-            delay: 5000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    });
-}
+//$('header').load("header.html");
+//$('footer').load("footer.html");
 
 /*Se agregan las animaciones para toda la pagina que no cargan de menera recurrente*/
 wow.init();
